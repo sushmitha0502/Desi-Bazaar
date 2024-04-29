66e6627d6ae4d0f1536783f9bd0dc1d6950e86ce
Sushmitha Botcha <118876855+sushmitha0502@users.noreply.github.com>
2024-04-29 22:48:44 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,75 @@
+import { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { Outlet } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import './App.css';
+import SummaryApi from './common';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Context from './context';
+import { setUserDetails } from './store/userSlice';
+
+function App() {
+  const dispatch = useDispatch()
+  const [cartProductCount,setCartProductCount] = useState(0)
+
+  const fetchUserDetails = async()=>{
+    try {
+      const dataResponse = await fetch(SummaryApi.current_user.url,{
+        method : SummaryApi.current_user.method,
+        credentials : 'include'
+      })
+
+      const dataApi = await dataResponse.json()
+
+      if(dataApi.success){
+        dispatch(setUserDetails(dataApi.data))
+      }
+    } catch (error) {
+      console.error('Error fetching user details:', error);
+    }
+  };
+
+
+
+  const fetchUserAddToCart = async()=>{
+    const dataResponse = await fetch(SummaryApi.addToCartProductCount.url,{
+      method : SummaryApi.addToCartProductCount.method,
+      credentials : 'include'
+    })
+
+    const dataApi = await dataResponse.json()
+
+    setCartProductCount(dataApi?.data?.count)
+  }
+
+  useEffect(()=>{
+    /**user Details */
+    fetchUserDetails()
+    /**user Details cart product */
+    fetchUserAddToCart()
+
+  },[]);
+  return (
+    <>
+      <Context.Provider value={{
+          fetchUserDetails, // user detail fetch 
+          cartProductCount, // current user add to cart product count,
+          fetchUserAddToCart
+      }}>
+        <ToastContainer 
+          position='top-center'
+        />
+        
+        <Header/>
+        <main className='min-h-[calc(100vh-120px)] pt-16'>
+          <Outlet/>
+        </main>
+        <Footer/>
+      </Context.Provider>
+    </>
+  );
+}
+
+export default App;
